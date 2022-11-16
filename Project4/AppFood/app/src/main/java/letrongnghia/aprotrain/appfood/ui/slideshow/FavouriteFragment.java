e6e5c97cb4fc08b554003eacb0e87e365f01cf78
letrongnghia0211 <lunnghia0211@gmail.com>
2022-11-16 09:05:54 +0700

    first commit

---
+++ Project4/AppFood/app/src/main/java/letrongnghia/aprotrain/appfood/ui/slideshow/FavouriteFragment.java
@@ -0,0 +1,19 @@
+package letrongnghia.aprotrain.appfood.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import letrongnghia.aprotrain.appfood.R;
+
+public class FavouriteFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_favourite, container, false);
+        return root;
+    }
+}
